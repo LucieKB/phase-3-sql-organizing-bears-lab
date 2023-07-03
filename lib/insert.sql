bf6f531b7dd4b4c7e467def26c8fb06c4080e394
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2023-07-03 16:48:46 -0600

    commit tree d06a39cae9cec36efb9137611c7d8281f40f2278

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "M.", "Blue", "Nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M.", "Purple", "Depressed", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 15, "F.", "Yellow", "Naughty", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 3, "F.", "Pink", "Brave", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 6, "M.", "Green", "Ambiguous", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 34, "M.", "Green", "Mean", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 9, "F.", "Teal", "Deceiptive", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 10, "M.", "Transparent", "Shy", 1);
